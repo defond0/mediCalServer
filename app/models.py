4f044cc44d3f97b9c8a58450fa9f08049c385adc
Jeff DeFond <jeffdefond@gmail.com>
2014-11-16 12:26:18 -0800

    jsonApi is now up,think this should make android json requests easy to handle

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 
 
 class Pill(db.Model):
+	__tablename__="pill"
 	id = db.Column(db.Integer, primary_key = True)
 	tube = db.Column(db.String(64), index = True)
 	name = db.Column(db.String(64), index = True)
@@ -13,9 +14,29 @@ class Pill(db.Model):
 		p = Pill(tube=tube,name=name,dose= dose, load=load)
 		db.session.add(p)
 		db.session.commit()
-		return p 
+		return p
+
+	@staticmethod
+	def deletePill(id):
+		pills = Pill.query.filter_by(id=id).all()
+		for p in pills:
+			db.session.delete(p)
+		db.session.commit()
+		return pills 
+
+	@property
+	def serialize(self):
+		return {
+           'id': self.id,
+           'tube': self.tube,
+           'name': self.name,
+           'dose': self.dose,
+           'load': self.load
+       }	
+	
 
 class Prescription(db.Model):
+	__tablename__="prescription"
 	id = db.Column(db.Integer, primary_key = True)
 	patient = db.Column(db.String(64), index = True)
 	rfid = db.Column(db.LargeBinary)
@@ -26,19 +47,54 @@ class Prescription(db.Model):
 		p = Prescription(patient=patient,rfid = rfids[rfid])
 		db.session.add(p)
 		db.session.commit()
-		return p 		
+		return p
+
+	@staticmethod
+	def deletePrescription(id):
+		prescriptions = Prescription.query.filter_by(id=id).all()
+		for p in prescriptions:
+			db.session.delete(p)
+		db.session.commit()
+		return prescriptions  
+
+	@property
+	def serialize(self):
+		return {
+           'id': self.id,
+           'patient': self.patient,
+           'rfid': self.rfid
+       }	
 
 class PPjoin(db.Model):
+	__tablename__="join"
 	id = db.Column(db.Integer, primary_key = True)
 	pillId = db.Column(db.Integer, db.ForeignKey('prescription.id'))
 	prescriptionId = db.Column(db.Integer, db.ForeignKey('prescription.id'))
 	times = db.Column(db.String(120), index = True)
 
 	@staticmethod
-	def createJoin(pillId, perscriptionId, times ):
-		j = PPjoin(pillId=pillId, prescriptionId=perscriptionId, times=times)
+	def createJoin(pillId, prescriptionId, times ):
+		j = PPjoin(pillId=pillId, prescriptionId=prescriptionId, times=times)
 		db.session.add(j)
 		db.session.commit()
-		return j 
+		return j
+
+	@staticmethod
+	def deleteJoin(id):
+		joins = PPjoin.query.filter_by(id=id).all()
+		for j in joins:
+			db.session.delete(j)
+		db.session.commit()
+		return joins  
+
+	@property
+	def serialize(self):
+		return {
+           'id': self.id,
+           'pillId': self.pillId,
+           'prescriptionId': self.prescriptionId,
+           'times': self.times
+       }	
+	 
 
 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
+from flask import Flask, render_template, flash, redirect, session, url_for, request, g, json, jsonify
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, models
 from .forms import pillForm, prescriptionForm, addPillsForm #, loginForm
@@ -9,24 +9,21 @@ def index():
 	pills = models.Pill.query.all()
 	prescriptions = models.Prescription.query.all()
 	joins = models.PPjoin.query.all()
-	print joins 
 	return render_template("index.html", pills= pills , prescriptions = prescriptions, joins= joins, title='Medical Pillar')
 
 @app.route('/newpill', methods=['GET','POST'])
 def newPill():
 	form = pillForm()
 	if form.validate_on_submit():
-		print("fling")
 		p = models.Pill.createPill(form.tube.data, form.name.data, form.dose.data, form.load.data)
 		flash('Creating Pill "%s", in tube %s, of dose %s  with %s load' % (p.name, p.tube, p.dose, p.load))
 	return render_template("newPill.html", form=form, title='Create Pill')
 
-
 @app.route('/newprescription', methods=['GET','POST'])
 def newPrescription():
 	form = prescriptionForm()
+	request.content_type
 	if form.validate_on_submit():
-		print("ding")
 		pr = models.Prescription.createPrescription(form.patient.data, form.rfid.data)
 	 	flash('Creating Prescription for "%s" with rfid %s' %(pr.patient, pr.rfid))
 	return render_template('newPrescription.html', form=form, title='Create Prescription')
@@ -34,13 +31,57 @@ def newPrescription():
 @app.route('/addpills', methods=['GET','POST'])
 def addPills():
 	form = addPillsForm()
-	print form.errors
-	print form.pills.data
-	print form.prescriptions.data
-	print form.times.data
-	# if form.validate_on_submit():
-	print("ping")
 	j=models.PPjoin.createJoin(form.pills.data, form.prescriptions.data, form.times.data)
-	flash('Adding pill "%s" to perscription %s at time %s'  %(j.pillId, j.prescriptionId, j.times))
+	flash('Adding pill "%s" to prescription %s at time %s'  %(j.pillId, j.prescriptionId, j.times))
 	return render_template('addPills.html', form=form, title='Add Pills to Perscriptions')
 
+@app.route('/api/getallpills', methods=['GET','POST'])
+def apiGetAllPills():
+	pills = models.Pill.query.all()		
+	return jsonify(pill_list=[ p.serialize for p in pills])
+
+@app.route('/api/getallprescriptions', methods=['GET','POST'])
+def apiGetAllPrescriptions():
+	prescriptions = models.Prescription.query.all()		
+	return jsonify(prescription_list=[ p.serialize for p in prescriptions])
+
+@app.route('/api/getalljoins', methods=['GET','POST'])
+def apiGetAllJoins():
+	joins = models.PPjoin.query.all()		
+	return jsonify(join_list=[ j.serialize for j in joins])
+
+@app.route('/api/newpill', methods=['GET','POST'])
+def apiNewPill():
+	args = request.args
+	p = models.Pill.createPill(args['tube'],args['name'],args['dose'],args['load'])
+	return jsonify(new_pill_list=[p.serialize])
+
+@app.route('/api/newprescription', methods=['GET','POST'])
+def apiNewPrescription():
+	args = request.args
+	p = models.Prescription.createPrescription(args['patient'],args['rfid'])
+	return jsonify(new_prescription_list=[p.serialize])
+
+@app.route('/api/newjoin', methods=['GET','POST'])
+def apiNewJoin():
+	args = request.args
+	j = models.PPjoin.createJoin(args['pillId'],args['prescriptionId'],args['times'])
+	return jsonify(new_join_list=[j.serialize])
+
+@app.route('/api/deletepill', methods=['GET','POST'])
+def apiDeletePill():
+	args = request.args
+	deleted_pills = models.Pill.deletePill(args['id'])
+	return jsonify(deleted_pills_list=[p.serialize for p in deleted_pills])
+
+@app.route('/api/deleteprescription', methods=['GET','POST'])
+def apiDeletePrescription():
+	args = request.args
+	deleted_prescriptions = models.Prescription.deletePrescription(args['id'])
+	return jsonify(deleted_prescriptions_list=[p.serialize for p in deleted_prescriptions])
+
+@app.route('/api/deletejoin', methods=['GET','POST'])
+def apiDeletejoin():
+	args = request.args
+	deleted_joins = models.PPjoin.deleteJoin(args['id'])
+	return jsonify(deleted_join_list=[p.serialize for p in deleted_joins])
